f2760dd0746a80ca723e44c05a549a3563a25d5f
Nick Fisher <nick.fisher@avinium.com>
2024-06-10 22:56:16 +0800

    first commit

---
+++ lib/shared_audio_utils.dart
@@ -0,0 +1,6 @@
+
+library;
+
+export 'src/audio_service.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ lib/src/audio_service.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+import 'package:logging/logging.dart';
+import 'dart:typed_data';
+import 'dart:async';
+
+enum AudioSource { Asset, File }
+
+abstract class AudioService {
+  Logger get log => Logger(this.runtimeType.toString());
+
+  Future initialize();
+
+  void dispose();
+
+  ///
+  /// Loads (but does not decode) the audio located at the specified [path] (interpreted as either a file or asset path, depending on [source])
+  ///
+  Future<Uint8List> load(String path,
+      {AudioSource source = AudioSource.File,
+      String? package,
+      Function? onBegin,
+      int sampleRate = 16000});
+
+  ///
+  /// Plays the audio located at the specified [path] (interpreted as either a file or asset path, depending on [source])
+  ///
+  Future play(
+    String path, {
+    AudioSource source = AudioSource.File,
+    String? package,
+    Function? onBegin,
+    int sampleRate = 16000,
+    double speed = 1.0,
+  });
+
+  ///
+  /// Plays the 16 bit PCM audio contained in [data].
+  ///
+  Future playBuffer(Uint8List data, int frequency, bool stereo,
+      {void Function()? onComplete});
+
+  ///
+  /// Plays the 16 bit PCM audio contained in [data].
+  ///
+  Future playStream(
+      Stream<Uint8List> data, int frequency, bool stereo, 
+      {void Function()? onComplete});
+
+  ///
+  /// Decodes the provided audio based on the specified file extension.
+  ///
+  Future<Uint8List> decode(Uint8List encoded, {String extension = "opus"});
+}
