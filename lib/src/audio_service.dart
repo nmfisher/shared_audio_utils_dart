b2936f749d606098c6ad594af69fe613d72e02ba
Nick Fisher <nick.fisher@avinium.com>
2024-09-21 13:29:46 +0800

    AudioEncoding enum and getDuration method

--- lib/src/audio_service.dart
+++ lib/src/audio_service.dart
@@ -5,7 +5,52 @@ import 'dart:async';
 
 enum AudioSource { Asset, File }
 
-enum AudioEncoding { PCM16, OGG, OPUS, MP3 }
+enum AudioEncoding {
+  PCM16(name: 'PCM16', bitsPerSample: 16),
+  PCMF32(name: 'PCMF32', bitsPerSample: 32),
+  OGG(name: 'OGG'),
+  OPUS(name: 'OPUS'),
+  MP3(name: 'MP3'),
+  WAV(name: 'WAV'),
+  M4A(name: 'M4A');
+
+  const AudioEncoding({
+    required this.name,
+    this.bitsPerSample,
+    this.sampleRate,
+  });
+
+  final String name;
+  final int? bitsPerSample;
+  final int? sampleRate;
+
+  bool get isPCM => this == AudioEncoding.PCM16 || this == AudioEncoding.PCMF32;
+
+  static AudioEncoding fromExtension(String extension) {
+    switch (extension.toLowerCase()) {
+      case 'ogg':
+        return AudioEncoding.OGG;
+      case 'opus':
+        return AudioEncoding.OPUS;
+      case 'mp3':
+        return AudioEncoding.MP3;
+      case 'wav':
+        return AudioEncoding.WAV;
+      case 'm4a':
+        return AudioEncoding.M4A;
+      default:
+        throw ArgumentError('Unsupported file extension: $extension');
+    }
+  }
+
+  AudioEncoding withSampleRate(int sampleRate) {
+    return AudioEncoding.values.firstWhere(
+      (e) => e.name == this.name,
+      orElse: () => throw StateError('Invalid enum value'),
+    );
+  }
+}
+
 
 abstract class AudioService {
   Logger get log => Logger(this.runtimeType.toString());
@@ -13,6 +58,11 @@ abstract class AudioService {
   Future initialize();
 
   void dispose();
+
+  ///
+  ///
+  ///
+  Future<Duration> getDuration(Uint8List data, AudioEncoding encoding);
 
   ///
   /// Loads (but does not decode) the audio located at the specified [path] (interpreted as either a file or asset path, depending on [source])
@@ -30,23 +80,28 @@ abstract class AudioService {
     String path, {
     AudioSource source = AudioSource.File,
     String? package,
-    Function? onBegin,
-    Function? onComplete,
+    void Function()? onBegin,
+    void Function()? onComplete,
     int sampleRate = 16000,
     double speed = 1.0,
   });
 
   ///
   /// Plays the audio contained in [data] encoded with [encoding].
+  /// [start] must be a double between 0.0 and 1.0, representing the fraction of the audio length at which to start playing.
   ///
-  Future<void Function()> playBuffer(Uint8List data, int frequency, bool stereo,
-      {void Function()? onComplete, AudioEncoding encoding = AudioEncoding.PCM16});
+  Future<void Function()> playBuffer(Uint8List data,
+      {void Function()? onBegin,
+      void Function()? onComplete,
+      AudioEncoding encoding = AudioEncoding.PCM16,
+      int? sampleRate,
+      bool? stereo,
+      double? start});
 
   ///
   /// Plays the 16 bit PCM audio contained in [data].
   ///
-  Future playStream(
-      Stream<Uint8List> data, int frequency, bool stereo, 
+  Future playStream(Stream<Uint8List> data, int frequency, bool stereo,
       {void Function()? onComplete});
 
   ///
